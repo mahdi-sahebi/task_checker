2a34aa12bdb6607b9aed6ecada6380e63a9bddaa
Mahdi Sahebi <m.nejadsahebi@live.co.uk>
2024-11-08 22:53:57 +0330

    [UPDATE]: APIs are implemented

--- include/task_item/task_container.h
+++ include/task_item/task_container.h
@@ -3,13 +3,24 @@
 
 #include <QObject>
 #include <QVariantList>
+#include "task_item.h"
 
 
 class TaskContainer : public QObject
 {
     Q_OBJECT
+    Q_PROPERTY(QVariantList list READ GetList WRITE SetList NOTIFY OnListChanged)
+
 public:
     explicit TaskContainer(QObject* parent = nullptr);
+    ~TaskContainer();
+    Q_INVOKABLE void Add();
+    Q_INVOKABLE void Remove(const uint32_t index);
+    QVariantList GetList();
+    void SetList(const QVariantList& list);
+
+signals:
+    void OnListChanged();
 
 private:
     QVariantList list_;
--- src/task_item/task_container.cc
+++ src/task_item/task_container.cc
@@ -1,6 +1,44 @@
 #include <task_item/task_container.h>
 
+
 TaskContainer::TaskContainer(QObject* parent)
 {
     (void)parent;
+}
+
+TaskContainer::~TaskContainer()
+{
+}
+
+void TaskContainer::Add()
+{
+    TaskItem* const task_item = new TaskItem("hi", [](){return false;});
+    list_.append(QVariant::fromValue(task_item));
+    emit OnListChanged();
+}
+
+void TaskContainer::Remove(const uint32_t index)
+{
+    if (index <= static_cast<uint32_t>(list_.size() - 1)) {
+        TaskItem* const task_item = list_[index].value<TaskItem*>();
+        if (nullptr != task_item) {
+            delete task_item;
+        }
+
+        list_.removeAt(index);
+        emit OnListChanged();
+    }
+}
+
+QVariantList TaskContainer::GetList()
+{
+    return list_;
+}
+
+void TaskContainer::SetList(const QVariantList& list)
+{
+    if (list != list_) {
+        list_ = list;
+        emit OnListChanged();
+    }
 }
