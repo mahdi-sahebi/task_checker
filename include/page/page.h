b06e189f8af57f1acb68618c482a2ed1da109232
Mahdi Sahebi <m.nejadsahebi@live.co.uk>
2024-11-25 22:27:21 +0330

    [UPDATE]: Get page status APIs are implemented

--- include/page/page.h
+++ include/page/page.h
@@ -1,17 +1,43 @@
 #ifndef PAGE_H_
 #define PAGE_H_
 
+#include <cstdint>
+#include <QString>
 #include <QObject>
-#include <task_item/task_container.h>
+//#include <task_item/task_container.h>
 
 class Page : public QObject
 {
-Q_OBJECT
+    Q_OBJECT
+    Q_PROPERTY(QString title READ GetTitle WRITE SetTitle NOTIFY OnTitleChanged)
+    Q_PROPERTY(float progress READ GetProgress NOTIFY OnProgressChanged)
+    Q_PROPERTY(uint8_t tasks_count READ GetTasksCount NOTIFY OnTasksCountChanged)
 
 public:
+    Page();
+    Page(const Page&) = delete;
+    Page& operator=(const Page&) = delete;
+    ~Page();
+
+    QString GetTitle();
+    void SetTitle(const QString& title);
+    float GetProgress();
+    uint8_t GetTasksCount();
+
+signals:
+    void OnTitleChanged();
+    void OnProgressChanged();
+    void OnTasksCountChanged();
 
 private:
+    QString title_;
+    float progress_;// TODO(MN): Remove - calculate
+    uint8_t tasks_count_;
 //    TaskContainer task_container_;
+
+    void SetProgress(const float& percent);
+    void SetTasksCount(const uint8_t& count);
+
 };
 
 
--- src/page/page.cc
+++ src/page/page.cc
@@ -1,2 +1,47 @@
 #include "page/page.h"
 
+Page::Page() :
+    title_{""},
+    progress_{0.0F},
+    tasks_count_{0}
+{
+
+}
+
+Page::~Page()
+{
+
+}
+
+QString Page::GetTitle()
+{
+    return title_;
+}
+
+void Page::SetTitle(const QString& title)
+{
+    title_ = title;
+    emit OnTitleChanged();
+}
+
+float Page::GetProgress()
+{
+    return 0.0F;
+}
+
+void Page::SetProgress(const float& percent)
+{
+    progress_ = percent;
+    emit OnProgressChanged();
+}
+
+uint8_t Page::GetTasksCount()
+{
+    return tasks_count_;
+}
+
+void Page::SetTasksCount(const uint8_t& count)
+{
+    tasks_count_ = count;
+    emit OnTasksCountChanged();
+}
