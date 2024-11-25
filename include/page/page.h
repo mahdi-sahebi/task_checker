63f058dfd1659a6fe2593fb4c74e120e912901fc
Mahdi Sahebi <m.nejadsahebi@live.co.uk>
2024-11-25 11:18:15 +0330

    [ADD]: Page resources are created

---
+++ include/page/page.h
@@ -0,0 +1,18 @@
+#ifndef PAGE_H_
+#define PAGE_H_
+
+#include <QObject>
+#include <task_item/task_container.h>
+
+class Page : public QObject
+{
+Q_OBJECT
+
+public:
+
+private:
+//    TaskContainer task_container_;
+};
+
+
+#endif /* PAGE_H_ */
